dde89d48179769af85b4ba650623aeccf86a2bd2
ATpro <122941982+ATpro-Apps@users.noreply.github.com>
2024-09-16 16:20:49 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Select the button using its ID
+document.getElementById("start-btn").addEventListener("click", function() {
+    // Redirect to subjects.html
+    window.location.href = "subjects.html";
+});
